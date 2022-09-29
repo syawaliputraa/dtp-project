79b3845b672a00f29deb497e3000b85344c7b885
syawali <hafiddevils.123@gmail.com>
2022-09-29 14:50:57 +0700

    syawali

---
+++ resources/views/templates/PARTIALS/styles.blade.php
@@ -0,0 +1,5 @@
+<link href="./dist/css/tabler.min.css" rel="stylesheet"/>
+    <link href="./dist/css/tabler-flags.min.css" rel="stylesheet"/>
+    <link href="./dist/css/tabler-payments.min.css" rel="stylesheet"/>
+    <link href="./dist/css/tabler-vendors.min.css" rel="stylesheet"/>
+    <link href="./dist/css/demo.min.css" rel="stylesheet"/>
